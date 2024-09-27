a512c3c1775a41c9b8b8ecc702bab6e78bbfcd78
crpboy <crpboy2004@gmail.com>
2024-09-27 12:34:13 +0800

    upd

--- os/src/mm/heap_allocator.rs
+++ os/src/mm/heap_allocator.rs
@@ -5,11 +5,11 @@ use buddy_system_allocator::LockedHeap;
 
 #[global_allocator]
 /// heap allocator instance
-static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
+static HEAP_ALLOCATOR: LockedHeap = LockedHeap::new();
 
 #[alloc_error_handler]
 /// panic when heap allocation error occurs
-pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+pub fn heap_alloc_error_handler(layout: core::alloc::Layout) -> ! {
     panic!("Heap allocation error, layout = {:?}", layout);
 }
 
